695dc3482f8c6338eff3eefc082f6175c696016a
Trang Vu <93897591+trangvu32@users.noreply.github.com>
2022-10-24 14:20:38 +1000

    Update SQL Excercises.sql

--- Exploratory Data Analysis in SQL Course/SQL Excercises.sql
+++ Exploratory Data Analysis in SQL Course/SQL Excercises.sql
@@ -1,13 +1,34 @@
 Count missing values
 #Task 1
--- Select the count of industry, 
--- subtract from total number of rows, and alias as missing
 SELECT COUNT(*) - COUNT(industry) AS missing
 FROM fortune500;
+
+
 Join tables
 #Task 2
 SELECT company.name
--- Table(s) to select from
   FROM company
        INNER JOIN fortune500
        ON company.ticker = fortune500.ticker;
+       
+       
+Inner Join
+#Task 3
+SELECT company.name, tag_type.tag, tag_type.type
+  FROM company
+       INNER JOIN tag_company 
+       ON company.id = tag_company.company_id
+       INNER JOIN tag_type
+       ON tag_company.tag = tag_type.tag
+  WHERE type='cloud';
+
+
+Coalesce Function
+SELECT coalesce(industry, sector, 'Unknown') AS industry2,
+       COUNT(*) 
+  FROM fortune500 
+ GROUP BY industry2
+ ORDER BY COUNT DESC
+ LIMIT 1;
+
+
