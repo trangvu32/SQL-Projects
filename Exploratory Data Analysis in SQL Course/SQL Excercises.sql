dfd193f8acac3bc0899ce9e08b16df040c4f1588
Trang Vu <93897591+trangvu32@users.noreply.github.com>
2022-10-24 14:28:16 +1000

    SQL

--- Exploratory Data Analysis in SQL Course/SQL Excercises.sql
+++ Exploratory Data Analysis in SQL Course/SQL Excercises.sql
@@ -3,14 +3,12 @@ Count missing values
 SELECT COUNT(*) - COUNT(industry) AS missing
 FROM fortune500;
 
-
 Join tables
 #Task 2
 SELECT company.name
   FROM company
        INNER JOIN fortune500
        ON company.ticker = fortune500.ticker;
-       
        
 Inner Join
 #Task 3
@@ -22,7 +20,6 @@ SELECT company.name, tag_type.tag, tag_type.type
        ON tag_company.tag = tag_type.tag
   WHERE type='cloud';
 
-
 Coalesce Function
 SELECT coalesce(industry, sector, 'Unknown') AS industry2,
        COUNT(*) 
@@ -30,5 +27,16 @@ SELECT coalesce(industry, sector, 'Unknown') AS industry2,
  GROUP BY industry2
  ORDER BY COUNT DESC
  LIMIT 1;
+ 
+Coalesce with a self-join
+SELECT company_original.name, title, rank
+  FROM company AS company_original
+	   LEFT JOIN company AS company_parent
+       ON company_original.parent_id = company_parent.id 
+       INNER JOIN fortune500 
+       ON coalesce(company_parent.ticker, 
+                   company_original.ticker) = 
+             fortune500.ticker
+ ORDER BY rank; 
 
 
